59167589054c5e49bbd15e6da37d86eee82e71d0
FMABr <felipemuamorim@gmail.com>
2022-05-13 22:11:09 -0400

    Session Cookie options

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,28 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { ExpressAdapter } from '@nestjs/platform-express';
+
+import session from 'express-session';
 
 import { AppConfigService } from './app.config.service';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(),
-  );
+  const app = await NestFactory.create(AppModule);
 
   const appConfig = app.get(AppConfigService);
+
+  app.use(
+    session({
+      secret: 'secret',
+      saveUninitialized: false,
+      resave: false,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24,
+        httpOnly: true,
+        path: '/',
+      },
+    }),
+  );
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -22,4 +33,5 @@ async function bootstrap() {
 
   await app.listen(appConfig.port, appConfig.host);
 }
+
 bootstrap();
